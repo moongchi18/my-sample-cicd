7e2b8bcf328739de73be45a35b31beeb9115f6b3
Moongchi18 <103802126+Moongchi18@users.noreply.github.com>
2022-10-13 16:38:57 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,8 @@ PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/springboot-app.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/application.log"
-DEPLOY_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
@@ -16,4 +16,4 @@ echo "$TIME_NOW : run $JAR_FILE" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW : process id is $CURRENT_PID" >> $DEPLOY_LOG
+echo "$TIME_NOW : process id is $CURRENT_PID" >> $DEPLOY_LOG  
